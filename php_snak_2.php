5f8e52c85d6dcfc9c5b13bdccb38baf431942cdc
Shinnolo <simo.28.09o@gmai.com>
2021-02-25 14:38:21 +0100

    Primo controllo

---
+++ php_snak_2.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_GET['name']; 
+$mail = $_GET['mail'];
+$age = $_GET['age']
+
+if(empty($nome) || empty($email) || empty($eta)){
+    $message = 'Prego, inserire i dati richiesti';
+} 
+
+
+?>
