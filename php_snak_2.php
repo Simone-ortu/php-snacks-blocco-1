98fab762bf272b5e72f78352ec3b4848d9e7ee6d
Shinnolo <simo.28.09o@gmai.com>
2021-02-25 14:39:30 +0100

    Secondo controllo

--- php_snak_2.php
+++ php_snak_2.php
@@ -5,7 +5,10 @@ $age = $_GET['age']
 
 if(empty($nome) || empty($email) || empty($eta)){
     $message = 'Prego, inserire i dati richiesti';
-} 
+} elseif(strlen($nome) >= 3 && strlen($email) >= 3 && is_numeric($eta)){
+    if(strpos($email, '@')  && strpos($email, '.')){
+        $message = 'Accesso riuscito';
+    }
 
 
 ?>
