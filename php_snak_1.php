76286d51446d3ce1c919e5b3073db270e3edc99d
Shinnolo <simo.28.09o@gmai.com>
2021-02-25 14:11:38 +0100

    Array

---
+++ php_snak_1.php
@@ -0,0 +1,25 @@
+<?php
+$matches = [
+    [
+    $home= 'Olimpia Milano',
+    $visitors= 'Cantu',
+    $homePoints= 55,
+    $visitorsPoints = 60
+
+    ],
+    [
+    $home= 'Olimpia Milano',
+    $visitors= 'Reggiana',
+    $homePoints= 102,
+    $visitorsPoints = 70
+    ],
+    [
+    $home= 'Dinamo Sassari',
+    $visitors= 'VL Pesaro',
+    $homePoints= 99,
+    $visitorsPoints = 74
+    ]
+
+
+]
+?>
