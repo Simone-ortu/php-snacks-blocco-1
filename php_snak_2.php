4603ed60822db2f49f702c754634d83b86402f6a
Shinnolo <simo.28.09o@gmai.com>
2021-02-25 14:50:33 +0100

    Correzione problema

--- php_snak_2.php
+++ php_snak_2.php
@@ -1,14 +1,33 @@
 <?php
-$name = $_GET['name']; 
 $mail = $_GET['mail'];
-$age = $_GET['age']
-
-if(empty($nome) || empty($email) || empty($eta)){
-    $message = 'Prego, inserire i dati richiesti';
-} elseif(strlen($nome) >= 3 && strlen($email) >= 3 && is_numeric($eta)){
+$name = $_GET['name'];
+$age = $_GET['age'];
+if((empty($nome) || empty($email) || empty($eta))){
+    $message = 'Inserire i dati richiesti';
+}
+elseif(strlen($nome) >= 3 && strlen($email) >= 3 && is_numeric($eta)){
     if(strpos($email, '@')  && strpos($email, '.')){
         $message = 'Accesso riuscito';
     }
-
-
+}
+else{
+    $message= 'Accesso negato';
+}
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <?php echo($message)
+    ?>
+    </div>
+</body>
+</html>
